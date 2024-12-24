60caa96e508022f57559db173f7c341725151955
Kenobi98 <majilakartik123@gmail.com>
2024-12-24 10:00:25 +0530

    updated path to example

--- gata.py
+++ gata.py
@@ -1,7 +1,7 @@
 from main import Interaction
 
-struct_file = "../fold_e_gata3_dna_binding_domain_motif_c_terminus_model_0.cif"
-data_file = "../fold_e_gata3_dna_binding_domain_motif_c_terminus_full_data_0.json"
+struct_file = "./example/fold_e_gata3_dna_binding_domain_motif_c_terminus_model_0.cif"
+data_file = "./example/fold_e_gata3_dna_binding_domain_motif_c_terminus_full_data_0.json"
 
 interactting_regions = {"A": [1, 10]}
 
