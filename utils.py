263c2c74293e89dc31fad4ccc417aa65040da16d
Kenobi98 <majilakartik123@gmail.com>
2024-12-25 10:16:08 +0530

    minor

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy.spatial import distance_matrix
 
 
@@ -5,9 +6,9 @@ def get_distance_map( coords1, coords2 ):
 	"""
 	Create an all-v-all distance map.
 	"""
-	distance_map = distance_matrix(  )
+	distance_map = distance_matrix( coords1, coords2 )
 
-	return get_distance_map
+	return distance_map
 
 
 def get_contact_map( distance_map, contact_threshold ):
