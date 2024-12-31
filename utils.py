aa029b9aabe12a7054ba45a5ba9d0be48471487c
Kenobi98 <majilakartik123@gmail.com>
2024-12-31 11:53:02 +0530

    added interaction_map()

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.spatial import distance_matrix
 
-def get_distance_map( coords1, coords2 ):
+def get_distance_map( coords1: np.array, coords2: np.array ):
 	"""
 	Create an all-v-all distance map.
 	"""
@@ -10,10 +10,31 @@ def get_distance_map( coords1, coords2 ):
 	return distance_map
 
 
-def get_contact_map( distance_map, contact_threshold ):
+def get_contact_map( distance_map: np.array, contact_threshold: float ):
 	"""
 	Given the distance map, create a binary contact map by thresholding distances.
 	"""
 	contact_map = np.where( distance_map <= contact_threshold, 1, 0 )
 
 	return contact_map
+
+
+def get_interaction_map( coords1: np.array, coords2: np.array, contact_threshold: float, map_type: str ):
+	"""
+	Create an interaction map, given the input coordinates.
+	Can create either of:
+		Distance map
+		Contact map
+	"""
+	distance_map = get_distance_map( coords1, coords2 )
+
+	if map_type == "distance":
+		return distance_map
+	
+	elif map_type == "contact":
+		contact_map = get_contact_map( distance_map, contact_threshold )
+		return contact_map
+	
+	else:
+		raise Exception( "Invalid map_type specified..." )
+
