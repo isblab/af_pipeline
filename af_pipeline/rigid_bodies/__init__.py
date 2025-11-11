c5740a74e4f2b5ad53d67ba427c2178a71f1c7d6
omgol411 <omgolatkar2001@gmail.com>
2025-11-11 18:30:03 +0530

    update submodule and fix rigidbodies network path

--- af_pipeline/rigid_bodies/__init__.py
+++ af_pipeline/rigid_bodies/__init__.py
@@ -8,7 +8,7 @@ Module to extract confident region from AlphaFold predictions
     <p>
         Double click on the node to go to the corresponding line in the source code.
     </p>
-    <iframe src="../docs/network_viz/network_rigid_bodies.html" width="800" height="600" frameborder="0">
+    <iframe src="../../docs/network_viz/network_rigid_bodies.html" width="800" height="600" frameborder="0">
     </iframe>
 </body>
 """
