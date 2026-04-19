d73510561649162f435dd1136a646227780710e2
omgol411 <omgolatkar2001@gmail.com>
2026-04-19 20:44:24 +0530

    update examples

--- examples/rank_af_predictions.py
+++ examples/rank_af_predictions.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from argparse import ArgumentParser
 from af_pipeline.rank_predictions.rank_af import get_job_set_dirs
 from af_pipeline.utils.file_utils import read_json, write_json
-from af_pipeline.rank_predictions.rank_af import RankAF3JobSet
+from af_pipeline.rank_predictions.rank_af import RankAF3JobSet, RankAF2JobSet
 from af_pipeline.constants.af_constants import ConfigYaml
 from af_pipeline.constants import af_constants
 
@@ -33,11 +33,22 @@ if __name__ == "__main__":
     )
 
     args.add_argument(
+        "-t",
+        "--pred_type",
+        type=str,
+        required=False,
+        default="AF3",
+        choices=["AF3", "AF2", "ColabFold"],
+        help="Type of predictions to create input jobs for. (Default: AF3)",
+    )
+
+    args.add_argument(
         "--pred_dirs",
         nargs="+",
         required=False,
         default=[
             "./input/AF_predictions/AF3",
+            # "./input/AF_predictions/ColabFold/Act1_1_1to375_Cdc3_1_11to120"
             # "./input/AF_predictions/AF3/Collagen_triple_helix_targets",
             # "./input/AF_predictions/AF3/Actin_profilin_targets/Act1_1_1-375_Cdc3_1_1-127",
             # "./input/AF_predictions/AF3/Actin_profilin_targets/actin_profilin_complex",
@@ -55,27 +66,47 @@ if __name__ == "__main__":
 
     config_dict = read_json(args.input)
 
+    # Get the best prediction for each job set directory
+    best_predictions = {}
+
     job_set_dirs = set()
 
     # scan through all directories and subdirectories to find job set directories
     for pred_dir in args.pred_dirs:
-        print(f"Scanning directory: {pred_dir}")
         if not os.path.exists(pred_dir):
             warnings.warn(f"Prediction directory {pred_dir} does not exist. Skipping...")
             continue
-        job_set_dirs.update(get_job_set_dirs(pred_dir))
-
-    # Get the best prediction for each job set directory
-    best_predictions = {}
+        job_set_dirs.update(
+            get_job_set_dirs(pred_dir=pred_dir, pred_type=args.pred_type)
+        )
 
     for job_set_dir in list(set(job_set_dirs)):
-        ranker = RankAF3JobSet(
-            job_set_dir=job_set_dir,
-            af_input_jobs=config_dict.get(ConfigYaml.AF_INPUT_JOBS, {}),
-            soft_match=True,
-        )
-        best_pred_info = ranker.extract_af3_best_pred_data()
-        print("*"*50)
+
+        if args.pred_type == "AF3":
+            ranker = RankAF3JobSet(
+                job_set_dir=job_set_dir,
+                af_input_jobs=config_dict.get(ConfigYaml.AF_INPUT_JOBS, {}),
+                soft_match=True,
+            )
+            best_pred_info = ranker.extract_af3_best_pred_data()
+
+        elif args.pred_type == "AF2":
+            ranker = RankAF2JobSet(
+                job_set_dir=job_set_dir,
+                af_input_jobs=config_dict.get(ConfigYaml.AF_INPUT_JOBS, {}),
+                soft_match=True,
+                data_format="json",
+            )
+            best_pred_info = ranker.extract_af2_best_pred_data()
+
+        elif args.pred_type == "ColabFold":
+            ranker = RankAF2JobSet(
+                job_set_dir=job_set_dir,
+                af_input_jobs=config_dict.get(ConfigYaml.AF_INPUT_JOBS, {}),
+                soft_match=True,
+            )
+            best_pred_info = ranker.extract_colabfold_best_pred_data()
+
         if len(best_pred_info) > 0:
             best_predictions.update(best_pred_info)
 
@@ -84,3 +115,4 @@ if __name__ == "__main__":
         file_path=os.path.join(args.output, "best_af_predictions.json"),
         data=best_predictions,
     )
+    print(f"Best predictions for {len(best_predictions)} job sets written to {os.path.join(args.output, 'best_af_predictions.json')}")
